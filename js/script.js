06ab2f46ed599ac4784f1d50a4ff592e71867f65
HBurrinha <99562703+HBurrinha@users.noreply.github.com>
2022-03-20 16:27:43 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -82,7 +82,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // On first load, show home view
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
-  allCategoriesUrl, homeHtmlUrl, buildAndShowHomeHTML,
+  allCategoriesUrl, homeHtmlUrl,
   function (responseText) {
     document.querySelector("#main-content").innerHTML = responseText;
   },// ***** <---- TODO: STEP 1: Substitute [...] ******
@@ -93,7 +93,7 @@ $ajaxUtils.sendGetRequest(
 
 // Builds HTML for the home page based on categories array
 // returned from the server.
-var buildAndShowHomeHTML = function buildAndShowHomeHTML (categories) {
+function buildAndShowHomeHTML (categories) {
 
   // Load home snippet page
   $ajaxUtils.sendGetRequest(
